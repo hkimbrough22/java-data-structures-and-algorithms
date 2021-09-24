4547082d67c75b2f80a3c910f6c961aac12923bc
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-09-24 15:53:15 -0700

    adds initial setup for linkedLists

---
+++ codechallenges/lib/src/main/java/codechallenges/linkedList/LinkedList.java
@@ -0,0 +1,25 @@
+
+package codechallenges.linkedList;
+
+import org.w3c.dom.Node;
+
+public class LinkedList
+{
+    Node head = null;
+    Node tail = null;  // not strictly required
+
+    public void insert(int value)
+    {
+        // TODO: implement me
+    }
+    public boolean includes(int value)
+    {
+        return false;  // TODO: implement me
+    }
+
+    @Override
+    public String toString()
+    {
+        return "null";  // TODO: implement me
+    }
+}
