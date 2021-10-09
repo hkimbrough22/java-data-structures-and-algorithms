a171d41ad0517bf595e6691babc20266d8048c1b
Haustin Kimbrough <haustin.kimbrough@hotmail.com>
2021-10-08 20:12:10 -0500

    adds some of Binary search tree

--- codechallenges/lib/src/main/java/codechallenges/trees/BinarySearchTree.java
+++ codechallenges/lib/src/main/java/codechallenges/trees/BinarySearchTree.java
@@ -1,14 +1,24 @@
 package codechallenges.trees;
 
-public class BinarySearchTree<T extends Comparable<? super T>> extends BinaryTree<T>
-{
-    public void add(T value)
-    {
-        // TODO: implement me
+public class BinarySearchTree<T extends Comparable<? super T>> extends BinaryTree<T> {
+    public void add(T value) {
+        Node<T> newNode = new Node<>(value);
+        Node<T> node = root;
+        while(true){
+            if(newNode.value.compareTo(node.value) < 0) {
+                if(node.leftNode != null) {
+                    node =  node.leftNode;
+                    add(node.value);
+                }
+            }
+
+            if (newNode.value.compareTo(root.value) > 0) {
+                //do stuff
+            }
+        }
     }
 
-    public boolean contains (T value)
-    {
+    public boolean contains(T value) {
         // TODO: implement me
         return false;
     }
--- codechallenges/lib/src/main/java/codechallenges/trees/BinaryTree.java
+++ codechallenges/lib/src/main/java/codechallenges/trees/BinaryTree.java
@@ -1,33 +1,53 @@
 package codechallenges.trees;
 
+import java.util.ArrayList;
+
 public class BinaryTree<T>
 {
-    Node <T> root;
+     Node <T> root;
 
-    public Object[] preOrderTraversal()
+    public Object[] preOrderTraversal(Node<T> _node)
     {
         Object[] outputValues = null;
-        // Should return something like "A B C D"
-        // Can use an ArrayList internally
-        // ArrayList.toArray() returns Object[], hence why Object[] is the return type
-        // outputValues = preOrder(root);
-        // TODO: implement me
+        ArrayList<T> internalValues = new ArrayList<>();
+        internalValues.add(_node.value);
+        if(_node.leftNode != null) {
+            preOrderTraversal(_node.leftNode);
+        }
+        if(_node.rightNode != null) {
+            preOrderTraversal(_node.rightNode);
+        }
+        outputValues = internalValues.toArray();
         return outputValues;
     }
 
-    public Object[] inOrderTraversal()
+    public Object[] inOrderTraversal(Node<T> _node)
     {
         Object[] outputValues = null;
-        // outputValues = inOrder(root);
-        // TODO: implement me
+        ArrayList<T> internalValues = new ArrayList<>();
+        if(_node.leftNode != null) {
+            preOrderTraversal(_node.leftNode);
+        }
+        internalValues.add(_node.value);
+        if(_node.rightNode != null) {
+            preOrderTraversal(_node.rightNode);
+        }
+        outputValues = internalValues.toArray();
         return outputValues;
     }
 
-    public Object[] postOrderTraversal()
+    public Object[] postOrderTraversal(Node<T> _node)
     {
         Object[] outputValues = null;
-        // outputValues = postOrder(root);
-        // TODO: implement me
+        ArrayList<T> internalValues = new ArrayList<>();
+        if(_node.leftNode != null) {
+            preOrderTraversal(_node.leftNode);
+        }
+        if(_node.rightNode != null) {
+            preOrderTraversal(_node.rightNode);
+        }
+        internalValues.add(_node.value);
+        outputValues = internalValues.toArray();
         return outputValues;
     }
 }
